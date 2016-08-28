4847cb3622bb0f7a6f7f921f2082170586bd70be
Vanderson Nunes <vann.nunes@gmail.com>
2016-08-28 18:59:23 -0300

    Comentarios OK

---
+++ public/assets/js/app/src/disqus.js
@@ -0,0 +1,12 @@
+var disqus_config = function () {
+	this.page.url = DISQUS_PAGE_URL;  
+	this.page.identifier = DISQUS_PAGE_IDENTIFIER;
+	this.language = "pt_BR";
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = '//c3projetos.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
