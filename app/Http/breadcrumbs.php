0d744d97e15c5500be3fffd8fddf31e532eb33fb
Vanderson Nunes <vann.nunes@gmail.com>
2016-08-28 18:44:21 -0300

    Breadcrumbs OK

--- app/Http/breadcrumbs.php
+++ app/Http/breadcrumbs.php
@@ -11,7 +11,7 @@ BreadCrumbs::register('admin.project.create',function($breadcrumbs){
 
 BreadCrumbs::register('admin.project.update',function($breadcrumbs, $project){
 	$breadcrumbs->parent('admin.user.projects');
-	$breadcrumbs->push('Editar Projeto',route('admin.project.edit',['id' => $project->id]));
+	$breadcrumbs->push($project->title,route('admin.project.edit',['id' => $project->id]));
 });
 
 BreadCrumbs::register('admin.user.projects',function($breadcrumbs){
@@ -23,3 +23,20 @@ BreadCrumbs::register('admin.user.profile',function($breadcrumbs, $user){
 	$breadcrumbs->parent('admin.home');
 	$breadcrumbs->push($user->name, route('admin.user.profile') );
 });
+
+BreadCrumbs::register('admin.project.blog.post-list', function($breadcrumbs, $project){
+	$breadcrumbs->parent('admin.project.update', $project);
+	$breadcrumbs->push('Lista de Posts', route('admin.project.posts', ['projectId' => $project->id]));
+});
+
+BreadCrumbs::register('admin.project.blog.post.create', function($breadcrumbs, $project){
+	$breadcrumbs->parent('admin.project.blog.post-list', $project);
+	$breadcrumbs->push('Novo Post', route('admin.project.post.create', ['projectId' => $project->id]));
+});
+
+BreadCrumbs::register('admin.project.blog.post.update', function($breadcrumbs, $project, $post){
+	$breadcrumbs->parent('admin.project.blog.post-list', $project);
+	$breadcrumbs->push('Editar Post', route('admin.project.post.edit', ['projectId' => $project->id, 'id' => $post->id]));
+});
+
+
