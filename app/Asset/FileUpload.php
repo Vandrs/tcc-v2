2068fbc6a536cd92083902b36e88e717dcd1c932
Vanderson Nunes <vann.nunes@gmail.com>
2016-10-24 23:23:37 -0200

    RAtiado

--- app/Asset/FileUpload.php
+++ app/Asset/FileUpload.php
@@ -7,7 +7,7 @@ class FileUpload extends AssetBundle{
     protected $priority = 3;    
     public function __construct() {
         $basePath = url('/');
-        $this->baseJsPath = $basePath.'/assets/js/fileUpload/src/'
+        $this->baseJsPath = $basePath.'/assets/js/fileUpload/src/';
         $this->jsfiles = ['jquery-ui-widget.js','jquery-fileupload.js','jquery-iframe-transport.js'];
     }
 }
