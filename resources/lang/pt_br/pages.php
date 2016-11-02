fc0ecb32934ef1ee28244e7d43e16c91193cd39a
Vanderson Nunes <vann.nunes@gmail.com>
2016-11-02 10:25:30 -0200

    Ajustes

--- resources/lang/pt_br/pages.php
+++ resources/lang/pt_br/pages.php
@@ -3,7 +3,7 @@
 return [
 	'home' => [
 		'title' 	  => 'Plaforma colaborativa para desenvolvimento, gerencimento e validação de projetos',
-		'description' => 'A Plataforma C3 Projetos é uma platamforma colaborativa que auxilia seus usuários na divulgação, elaboração e validação de seus projetos e ideias',
+		'description' => 'A Plataforma C3 Projetos é uma plataforma colaborativa que auxilia seus usuários na divulgação, elaboração e validação de seus projetos e ideias',
 		'keywords' 	  => 'Gerenciamento de Projetos, Projetos Colaborativos, Desenvolvimento e Validação de Projetos'
 	],
 	'search' => [
