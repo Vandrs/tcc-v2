79ac88390366e4af0927109cd36e28ffa3e27bbe
Vanderson Nunes <vann.nunes@gmail.com>
2016-11-10 22:39:22 -0200

    Ajuste

--- public/assets/js/app/src/validationReports.js
+++ public/assets/js/app/src/validationReports.js
@@ -238,50 +238,50 @@ function buildPieReport(ctx, labels, data){
 					    chartInstance = this.chart,
 					    ctx = chartInstance.ctx;
 
-					ctx.font = '18px Arial';
-					ctx.textAlign = "center";
-					ctx.fillStyle = "#ffffff";
+						ctx.font = '18px Arial';
+						ctx.textAlign = "center";
+						ctx.fillStyle = "#ffffff";
 
-					Chart.helpers.each(self.data.datasets.forEach(function (dataset, datasetIndex) {
-					    var meta = self.getDatasetMeta(datasetIndex),
-					        total = 0, //total values to compute fraction
-					        labelxy = [],
-					        offset = Math.PI / 2, //start sector from top
-					        radius,
-					        centerx,
-					        centery, 
-					        lastend = 0; //prev arc's end line: starting with 0
+						Chart.helpers.each(self.data.datasets.forEach(function (dataset, datasetIndex) {
+						    var meta = self.getDatasetMeta(datasetIndex),
+						        total = 0, //total values to compute fraction
+						        labelxy = [],
+						        offset = Math.PI / 2, //start sector from top
+						        radius,
+						        centerx,
+						        centery, 
+						        lastend = 0; //prev arc's end line: starting with 0
 
-					    for (var val of dataset.data) { total += val; } 
+						    for (var val of dataset.data) { total += val; } 
 
-					    Chart.helpers.each(meta.data.forEach( function (element, index) {
-					        radius = 0.9 * element._model.outerRadius - element._model.innerRadius;
-					        centerx = element._model.x;
-					        centery = element._model.y;
-					        var thispart = dataset.data[index],
-					            arcsector = Math.PI * (2 * thispart / total);
-					        if (element.hasValue() && dataset.data[index] > 0) {
-					          labelxy.push(lastend + arcsector / 2 + Math.PI + offset);
-					        }
-					        else {
-					          labelxy.push(-1);
-					        }
-					        lastend += arcsector;
-					    }), self)
+						    Chart.helpers.each(meta.data.forEach( function (element, index) {
+						        radius = 0.9 * element._model.outerRadius - element._model.innerRadius;
+						        centerx = element._model.x;
+						        centery = element._model.y;
+						        var thispart = dataset.data[index],
+						            arcsector = Math.PI * (2 * thispart / total);
+						        if (element.hasValue() && dataset.data[index] > 0) {
+						          labelxy.push(lastend + arcsector / 2 + Math.PI + offset);
+						        }
+						        else {
+						          labelxy.push(-1);
+						        }
+						        lastend += arcsector;
+						    }), self)
 
-					    var lradius = radius * 3 / 4;
-					    for (var idx in labelxy) {
-					      if (labelxy[idx] === -1) continue;
-					      var langle = labelxy[idx],
-					          dx = centerx + lradius * Math.cos(langle),
-					          dy = centery + lradius * Math.sin(langle),
-					          val = Math.round(dataset.data[idx] / total * 100);
-					      ctx.fillText(val + '%', dx, dy);
-					    }
+						    var lradius = radius * 3 / 4;
+						    for (var idx in labelxy) {
+						      if (labelxy[idx] === -1) continue;
+						      var langle = labelxy[idx],
+						          dx = centerx + lradius * Math.cos(langle),
+						          dy = centery + lradius * Math.sin(langle),
+						          val = Math.round(dataset.data[idx] / total * 100);
+						      ctx.fillText(val + '%', dx, dy);
+						    }
 
-					}), self);
+						}), self);
 					}
-					},
+				}
 	        }    
 		});
 	ctx.data("chart",barChart);
